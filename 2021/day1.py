ee04948c128a374efa93744b1e7e72c9e8a8abc9
equan <equan06@gmail.com>
2021-12-01 20:21:44 -0800

    it begins

---
+++ 2021/day1.py
@@ -0,0 +1,23 @@
+import numpy as np
+from timeit import default_timer as timer
+
+def load_data():
+    with open("input_day1.txt") as f:
+        return [int(x) for x in f.read().splitlines()]
+
+def day1_sol1(input):
+    return sum(1 if x > 0 else 0 for x in np.diff(input))
+
+def day1_sol2(input):
+    return sum(i < j for i, j in zip(input[:-3], input[3:]))
+
+def day1_sol3(input):
+    acc = 0
+    for i in range(len(input)-2):
+        acc += input[i] < input[i + 2]     
+    return acc
+
+if __name__ == '__main__':
+    input = load_data()
+    print(day1_sol1(input))
+    print(day1_sol2(input))
