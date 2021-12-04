940e7f11256ecd0a63abb55a55b4c7471e48b33e
equan <equan06@gmail.com>
2021-12-03 22:34:40 -0800

    code cleanup

--- 2021/day4.py
+++ 2021/day4.py
@@ -2,31 +2,44 @@ import numpy as np
 import re
 from functools import reduce
 
+# first attempt - ugly, but it worked...
 def load_data():
     with open("input_day4.txt") as f:
-        # oh god.
         arr = [re.sub(r'\s+', ',', re.sub(r'^\s+', '', y, flags=re.MULTILINE)).split(",") for x in f.read().splitlines() for y in x.split("\r\n") ]
         input = list(map(int, arr[0]))
         print(arr)
         print(input)
         boards = []
-        board = []
+        curr = []
         for line in arr[1:]:
             if len(line) == 1:
-                if len(board) > 0:
-                    boards.append(board)
-                board = []
+                if len(curr) > 0:
+                    boards.append(curr)
+                curr = []
             else:
-                board.append(list(map(int, line)))
-        boards.append(board)
+                curr.append(list(map(int, line)))
+        boards.append(curr)
         print(boards)
         return input, boards
 
 
-# gotta practice some regex...
+# going back and practicing some regex
 def load_data_regex():
-    with open("input_day2.txt") as f:
-        return [(a, int(b)) for a, b in re.findall(r'^(\w+)\s(\d+)$', f.read().strip(), flags=re.MULTILINE)]
+    with open("input_day4.txt") as f:
+        arr = [list(map(int, re.findall(r'\d+', x))) for x in f.read().splitlines() for y in x.split("\r\n")]
+        input = arr[0]
+        boards = []
+        curr = []
+        for line in arr[1:]:
+            if len(line) == 0:
+                if len(curr) > 0:
+                    boards.append(curr)
+                curr = []
+            else:
+                curr.append(line)    
+        boards.append(curr)
+        return input, boards
+
 
 # check for bingo
 def score_board(board, marked):
@@ -49,39 +62,32 @@ def day1_sol1(input, boards):
                         tracker[idb].add((x,y))
             # find first bingo and score the unmarked squares
             if score_board(b, tracker[idb]):
-                score = 0
-                for x in range(n):
-                    for y in range(n):
-                        if (x, y) not in tracker[idb]:
-                            score += b[x][y]
+                score = sum(b[x][y] for x in range(n) for y in range(n) if (x, y) not in tracker[idb])
                 return score * num
     return -1
 
 def day1_sol2(input, boards):
     tracker = [set() for _ in boards]
+    n = len(boards[0])
     skip_board = set() # boards to skip once bingo accomplished
     for num in input:
         for idb, b in enumerate(boards):
-            if (idb in skip_board):
+            if idb in skip_board:
                 continue
-            for x in range(len(b)):
-                for y in range(len(b)):
+            for x in range(n):
+                for y in range(n):
                     if (b[x][y] == num):
                         tracker[idb].add((x,y))
             if score_board(b, tracker[idb]):
                 skip_board.add(idb)
                 # when the last bingo is found, score the board
-                if (len(skip_board) == len(boards)):
-                    score = 0
-                    for idx, x in enumerate(b):
-                        for idy, y in enumerate(x):
-                            if (idx, idy) not in tracker[idb]:
-                                score += y
+                if len(skip_board) == len(boards):
+                    score = sum(b[x][y] for x in range(n) for y in range(n) if (x, y) not in tracker[idb])
                     return score * num
     return -1
 
 
 if __name__ == '__main__':
-    input, boards = load_data()
+    input, boards = load_data_regex()
     print(day1_sol1(input, boards))
     print(day1_sol2(input, boards))
